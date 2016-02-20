42b4d20fc861ea19b031e2feffb25446322b8755
Stian Didriksen <stipsan@gmail.com>
2016-02-20 22:23:53 +0100

    added callback option instead of hard-relying on sendFile
    will add tests later

--- test/spec.js
+++ test/spec.js
@@ -3,6 +3,7 @@
 import fallback from '../lib'
 
 const args = ['index.html', { root: __dirname }]
+const callback = (req, res, next) => res.send()
 
 describe('constructor', () => {
   it('returns something that looks like a middleware', () => {
@@ -12,6 +13,45 @@ describe('constructor', () => {
 })
 
 describe('middleware', () => {
+  const next = sinon.stub()
+  let middleware
+  beforeEach(() => {
+    middleware = fallback(...args)
+  })
+  it('accepts HTML requests', () => {
+    const req = { method: 'GET', accepts: sinon.stub().returns('html') }
+    const res = { sendFile: sinon.stub() }
+    expect(middleware(req, res, next)).to.be.undefined
+    expect(req.accepts).always.have.been.calledWithMatch('html')
+    expect(res.sendFile).always.have.been.calledWithMatch(...args)
+    expect(next).not.to.have.been.called
+  })
+  it('ignores non-HTML requests', () => {
+    const req = { method: 'GET', accepts: sinon.stub().returns('') }
+    const res = { sendFile: sinon.stub() }
+    expect(middleware(req, res, next)).to.be.undefined
+    expect(req.accepts).always.have.been.calledWithMatch('html')
+    expect(res.sendFile).not.to.have.been.called
+    expect(next).to.have.been.called
+  })
+  it('ignores non-GET requests', () => {
+    const req = { method: 'POST', accepts: sinon.stub() }
+    const res = { sendFile: sinon.stub() }
+    expect(middleware(req, res, next)).to.be.undefined
+    expect(req.accepts).not.to.have.been.called
+    expect(res.sendFile).not.to.have.been.called
+    expect(next).to.have.been.called
+  })
+  it('passes on errors if the default file can not be served', () => {
+    const req = { method: 'GET', accepts: sinon.stub().returns('html') }
+    const res = { sendFile: sinon.spy((path, options, callback) => callback(Error)) }
+    expect(middleware(req, res, next)).to.be.undefined
+    expect(req.accepts).always.have.been.calledWithMatch('html')
+    expect(res.sendFile).to.have.been.called
+    expect(next).to.have.been.called
+  })
+})
+describe('middleware with function callback', () => {
   const next = sinon.stub()
   let middleware
   beforeEach(() => {
