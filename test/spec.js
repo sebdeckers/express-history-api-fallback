f4dbc45b5d47d536b042a306adb2e2118692880a
Dalton Santos <daltones@msn.com>
2016-12-14 18:04:25 -0200

    Update tests for HEAD method support

--- test/spec.js
+++ test/spec.js
@@ -12,41 +12,48 @@ describe('constructor', () => {
 })
 
 describe('middleware', () => {
+  const methods = ['GET', 'HEAD']
   const next = sinon.stub()
   let middleware
   beforeEach(() => {
     middleware = fallback(...args)
   })
-  it('accepts HTML requests', () => {
-    const req = { method: 'GET', accepts: sinon.stub().returns('html') }
-    const res = { sendFile: sinon.stub() }
-    expect(middleware(req, res, next)).to.be.undefined
-    expect(req.accepts).always.have.been.calledWithMatch('html')
-    expect(res.sendFile).always.have.been.calledWithMatch(...args)
-    expect(next).not.to.have.been.called
+  methods.forEach(method => {
+    it(`accepts ${method} HTML requests`, () => {
+      const req = { method, accepts: sinon.stub().returns('html') }
+      const res = { sendFile: sinon.stub() }
+      expect(middleware(req, res, next)).to.be.undefined
+      expect(req.accepts).always.have.been.calledWithMatch('html')
+      expect(res.sendFile).always.have.been.calledWithMatch(...args)
+      expect(next).not.to.have.been.called
+    })  
   })
-  it('ignores non-HTML requests', () => {
-    const req = { method: 'GET', accepts: sinon.stub().returns('') }
-    const res = { sendFile: sinon.stub() }
-    expect(middleware(req, res, next)).to.be.undefined
-    expect(req.accepts).always.have.been.calledWithMatch('html')
-    expect(res.sendFile).not.to.have.been.called
-    expect(next).to.have.been.called
+  methods.forEach(method => {
+    it(`ignores ${method} non-HTML requests`, () => {
+      const req = { method, accepts: sinon.stub().returns('') }
+      const res = { sendFile: sinon.stub() }
+      expect(middleware(req, res, next)).to.be.undefined
+      expect(req.accepts).always.have.been.calledWithMatch('html')
+      expect(res.sendFile).not.to.have.been.called
+      expect(next).to.have.been.called
+    })
   })
-  it('ignores non-GET requests', () => {
+  methods.forEach(method => {
+    it(`passes on errors if the default file can not be served for ${method} requests`, () => {
+      const req = { method, accepts: sinon.stub().returns('html') }
+      const res = { sendFile: sinon.spy((path, options, callback) => callback(Error)) }
+      expect(middleware(req, res, next)).to.be.undefined
+      expect(req.accepts).always.have.been.calledWithMatch('html')
+      expect(res.sendFile).to.have.been.called
+      expect(next).to.have.been.called
+    })
+  })
+  it('ignores non-GET and non-HEAD requests', () => {
     const req = { method: 'POST', accepts: sinon.stub() }
     const res = { sendFile: sinon.stub() }
     expect(middleware(req, res, next)).to.be.undefined
     expect(req.accepts).not.to.have.been.called
     expect(res.sendFile).not.to.have.been.called
-    expect(next).to.have.been.called
-  })
-  it('passes on errors if the default file can not be served', () => {
-    const req = { method: 'GET', accepts: sinon.stub().returns('html') }
-    const res = { sendFile: sinon.spy((path, options, callback) => callback(Error)) }
-    expect(middleware(req, res, next)).to.be.undefined
-    expect(req.accepts).always.have.been.calledWithMatch('html')
-    expect(res.sendFile).to.have.been.called
     expect(next).to.have.been.called
   })
 })
